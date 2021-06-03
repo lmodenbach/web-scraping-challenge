ab78eecdd14d2c808e8f848dfe1d7f295c7b682c
lmodenbach <lmodenbach@gmail.com>
2021-06-03 14:27:15 -0700

    pre-trial .py apps

--- mars_app.py
+++ mars_app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
 import pymongo
+import scrape_mars
 
 app = Flask(__name__)
+
+scrape_dict = []
+
+@app.route("/")
+def index():
+    return render_template("index.html", dict=scrape_dict)
 
 
 @app.route("/scrape")
 def scrape():
-    
+    scrape_dict = scrape_mars.scrape()
 
 if __name__ == "__main__":
     app.run(debug=True)
