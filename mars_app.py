a74319311940e75d101a1b2184a1fd76601fa1fe
lmodenbach <lmodenbach@gmail.com>
2021-06-07 15:36:33 -0700

    index prepped, app tweaked...

--- mars_app.py
+++ mars_app.py
@@ -4,18 +4,23 @@ from scrape_mars import scrape
 
 app = Flask(__name__)
 
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.mars_db
+
 scrape_dict = []
 
 @app.route("/")
 def home():
-    scrape_dict = scrape()
+    scrape()
+    scrape_dict = db.mars.find()
     return render_template("index.html", scraped=scrape_dict)
 
 
-# @app.route("/scrape")
-# def scrape():
-#     scrape_dict = scrape()
-#     return render_template("index.html", scraped=scrape_dict)
+@app.route("/scrape")
+def scrape():
+    scrape_dict = scrape()
+    db.mars.insert_one(scrape_dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
