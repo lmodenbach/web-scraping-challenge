d6e6e833bb72917cb7dd6fc63ff3bc6a3426d4b5
lmodenbach <lmodenbach@gmail.com>
2021-06-07 17:50:13 -0700

    added detail_data.py

--- mars_app.py
+++ mars_app.py
@@ -8,17 +8,17 @@ conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
 db = client.mars_db
 
-scrape_dict = []
+scrape_dict = {}
 
 @app.route("/")
 def home():
-    scrape()
-    scrape_dict = db.mars.find()
-    return render_template("index.html", scraped=scrape_dict)
+    scraper()
+    scraped_mongo = db.mars.find()
+    return render_template("index.html", scraped=scraped_mongo)
 
 
 @app.route("/scrape")
-def scrape():
+def scraper():
     scrape_dict = scrape()
     db.mars.insert_one(scrape_dict)
 
